9d52578f889b7006d541786f68746ab85af6a3f0
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-07-27 17:05:41 +0300

    Added comments for the technique TDD as if you meant it bottom up with Specification by Example

--- java/test/Something.java
+++ java/test/Something.java
@@ -4,6 +4,8 @@ import static junit.framework.Assert.assertEquals;
 
 /**
  * Created by Adi Bolboacă on 7/27/14.
+ * Bottom-up with Specification By Example
+ * TDD as if you meant it
  */
 public class Something {
     @Test
