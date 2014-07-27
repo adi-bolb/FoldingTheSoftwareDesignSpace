2bb7ce1d0842d0770a23e5697c547f546204330e
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-07-27 16:56:32 +0300

    Added dummy test to show junit is working

--- java/test/Something.java
+++ java/test/Something.java
@@ -1,5 +1,13 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by Adi Bolboacă on 7/27/14.
  */
 public class Something {
+    @Test
+    public void someTest(){
+        assertEquals(1,2);
+    }
 }
