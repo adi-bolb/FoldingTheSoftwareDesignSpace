7cda6cf91ab8c17290dc57e0197b7a8f26adad8d
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-08-01 18:54:05 +0300

    step 2: extracted the expected and actual into variables and separated arrange, act, assert

--- java/test/Something.java
+++ java/test/Something.java
@@ -19,7 +19,11 @@ public class Something {
 
     @Test
     public void anUniverseWithACellOnTickTransformsIntoAnEmptyUniverse(){
-        assertEquals("empty universe", "not empty universe");
+        String expected = "empty universe";
+
+        String actual = "not empty universe";
+
+        assertEquals(expected, actual);
     }
 
     private String getEmptyUniverse() {
