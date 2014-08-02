3ad8e4a8c8ac992d6383970c7b8fbb55c8e3599a
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-08-02 11:50:17 +0300

    step 4: extracted method cell

--- java/test/Something.java
+++ java/test/Something.java
@@ -21,7 +21,7 @@ public class Something {
     @Test
     public void anUniverseWithACellOnTickTransformsIntoAnEmptyUniverse(){
         String expected = UniverseBuilder.getEmptyUniverse();
-        String universeWithOneCell = UniverseBuilder.getUniverse() + with() + " one cell";
+        String universeWithOneCell = UniverseBuilder.getUniverse() + with() + " one" + getCell();
         String inputUniverse = universeWithOneCell;
 
         String actual = inputUniverse.equals(universeWithOneCell) ? UniverseBuilder.getEmptyUniverse() : UniverseBuilder.getNotEmptyUniverse();
@@ -32,7 +32,7 @@ public class Something {
     @Test
     public void anUniverseWithTwoCellsOnTickTransformsIntoAnEmptyUniverse(){
         String expected = UniverseBuilder.getEmptyUniverse();
-        String universeWithTwoCells = UniverseBuilder.getUniverse() + with() + " two cells";
+        String universeWithTwoCells = UniverseBuilder.getUniverse() + with() + " two" + getCell();
         String inputUniverse = universeWithTwoCells;
 
         String actual = inputUniverse.equals(universeWithTwoCells) ? UniverseBuilder.getEmptyUniverse() : UniverseBuilder.getNotEmptyUniverse();
@@ -43,12 +43,16 @@ public class Something {
     @Test
     public void anUniverseWithThreeNotNeighbouringCellsOnTickTransformsIntoAnEmptyUniverse(){
         String expected = UniverseBuilder.getEmptyUniverse();
-        String universeWithThreeCellsNotNeighbours = UniverseBuilder.getUniverse() + with() + " three cells not neighbors";
+        String universeWithThreeCellsNotNeighbours = UniverseBuilder.getUniverse() + with() + " three" + getCell() + " not neighbors";
         String inputUniverse = universeWithThreeCellsNotNeighbours;
 
         String actual = inputUniverse.equals(universeWithThreeCellsNotNeighbours) ? UniverseBuilder.getEmptyUniverse() : UniverseBuilder.getNotEmptyUniverse();
 
         assertEquals(expected, actual);
+    }
+
+    private String getCell() {
+        return " cell";
     }
 
     private String with() {
