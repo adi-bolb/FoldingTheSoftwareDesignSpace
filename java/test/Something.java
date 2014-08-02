7bd4afc5ef6840ffea0b4e4ab2e3d09c34f9b0e5
Adi Bolboaca <adi_bolb123@yahoo.com>
2014-08-02 11:51:02 +0300

    step 4: renamed getCell to cell

--- java/test/Something.java
+++ java/test/Something.java
@@ -21,7 +21,7 @@ public class Something {
     @Test
     public void anUniverseWithACellOnTickTransformsIntoAnEmptyUniverse(){
         String expected = UniverseBuilder.getEmptyUniverse();
-        String universeWithOneCell = UniverseBuilder.getUniverse() + with() + " one" + getCell();
+        String universeWithOneCell = UniverseBuilder.getUniverse() + with() + " one" + cell();
         String inputUniverse = universeWithOneCell;
 
         String actual = inputUniverse.equals(universeWithOneCell) ? UniverseBuilder.getEmptyUniverse() : UniverseBuilder.getNotEmptyUniverse();
@@ -32,7 +32,7 @@ public class Something {
     @Test
     public void anUniverseWithTwoCellsOnTickTransformsIntoAnEmptyUniverse(){
         String expected = UniverseBuilder.getEmptyUniverse();
-        String universeWithTwoCells = UniverseBuilder.getUniverse() + with() + " two" + getCell();
+        String universeWithTwoCells = UniverseBuilder.getUniverse() + with() + " two" + cell();
         String inputUniverse = universeWithTwoCells;
 
         String actual = inputUniverse.equals(universeWithTwoCells) ? UniverseBuilder.getEmptyUniverse() : UniverseBuilder.getNotEmptyUniverse();
@@ -43,7 +43,7 @@ public class Something {
     @Test
     public void anUniverseWithThreeNotNeighbouringCellsOnTickTransformsIntoAnEmptyUniverse(){
         String expected = UniverseBuilder.getEmptyUniverse();
-        String universeWithThreeCellsNotNeighbours = UniverseBuilder.getUniverse() + with() + " three" + getCell() + " not neighbors";
+        String universeWithThreeCellsNotNeighbours = UniverseBuilder.getUniverse() + with() + " three" + cell() + " not neighbors";
         String inputUniverse = universeWithThreeCellsNotNeighbours;
 
         String actual = inputUniverse.equals(universeWithThreeCellsNotNeighbours) ? UniverseBuilder.getEmptyUniverse() : UniverseBuilder.getNotEmptyUniverse();
@@ -51,7 +51,7 @@ public class Something {
         assertEquals(expected, actual);
     }
 
-    private String getCell() {
+    private String cell() {
         return " cell";
     }
 
