7d027f16c5aea9933c7cc75681e99e76ee0b30aa
Adi Bolboaca <adi_bolb123@yahoo.com>
2015-02-02 12:50:14 +0200

    Step1: Added mockito as a mocking framework and made the first test compile. The test fails

--- java/test/UniverseViewTests.java
+++ java/test/UniverseViewTests.java
@@ -1,15 +1,25 @@
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class UniverseViewTests {
 
     @Test
     public void whenStartingAnEmptyUniverseIsShown(){
         UniverseView universeView = new UniverseView();
+        UniverseCollaborator universeCollaborator = mock(UniverseCollaborator.class);
 
         universeView.show();
 
-        // TODO: Add an assertion
+        verify(universeCollaborator).getUniverse();
+    }
+
+    private interface UniverseCollaborator {
+        Universe getUniverse();
+    }
+
+    private class Universe {
     }
 }
