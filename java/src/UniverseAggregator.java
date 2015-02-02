0a9c3c068fc2612738c2237f7fdc94a64df369e7
Adi Bolboaca <adi_bolb123@yahoo.com>
2015-02-02 13:11:45 +0200

    Step1: addedUniverseBuilder as an dependency to UniverseAggregator

--- java/src/UniverseAggregator.java
+++ java/src/UniverseAggregator.java
@@ -2,6 +2,11 @@
  * Created by adi on 2/2/15.
  */
 public class UniverseAggregator implements UniverseCollaborator {
+
+    public UniverseAggregator(UniverseBuilder universeBuilder){
+
+    }
+
     @Override
     public Universe getUniverse() {
         return null;
