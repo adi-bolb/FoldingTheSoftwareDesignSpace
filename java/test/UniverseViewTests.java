8bee7be70f6808b4f0c9cbc7186725c0da51b9df
Adi Bolboaca <adi_bolb123@yahoo.com>
2015-02-02 12:43:58 +0200

    Added the first partial test for UniverseView. When starting an empty universe is shown

---
+++ java/test/UniverseViewTests.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class UniverseViewTests {
+    @Test
+    public void whenStartingAnEmptyUniverseIsShown(){
+        UniverseView universeView = new UniverseView();
+
+        universeView.show();
+
+        // TODO: Add an assertion
+    }
+}
